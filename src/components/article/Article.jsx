ba0d5f353fa3959c56a87ecb71fa9164ec87cb68
Dowson <dcheung@smail.pembinatrails.ca>
2022-04-15 20:52:35 -0500

    first commit

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./article.css";
 
-const Article = () => {
+//We get the "imgUrl" image and we can pass it through props to change the image for each and every article.
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
