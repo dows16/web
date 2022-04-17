64d775e94eeb319fa3c14f5ff5b351f589a18588
Dowson <dcheung@smail.pembinatrails.ca>
2022-04-17 17:09:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+//I imported all the components and containers I need into the main file below.
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+//I imported the CSS file that I need for the main gradient background color, as well as the proper media queries so it'll be accessible on mobile and tablet as well.
 import "./App.css";
 
 const App = () => {
